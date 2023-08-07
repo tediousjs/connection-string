369a63f03f263d81f576fb9f38afd80cf3b95f96
Dan Hensby <git@hens.by>
2023-08-07 21:56:45 +0200

    feat: add connection string builder

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,8 @@
 import parseConnectionString from './parser/connection-string';
 import parseSqlConnectionString from './parser/sql-connection-string';
 
+export * from './builder';
+
 export {
     parseConnectionString,
     parseSqlConnectionString,
